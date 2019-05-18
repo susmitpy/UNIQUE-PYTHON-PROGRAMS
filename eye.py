516476fa7251379b8c88b5b555794214d8096966
Susmit <susmit.py@gmail.com>
2019-05-18 19:00:12 +0530

    Create eye.py

---
+++ eye.py
@@ -0,0 +1,37 @@
+from time import sleep
+import os
+from threading import Thread
+
+class fiveMinuteBreak(Thread):
+    def run(self):
+        sleep(60*60)
+        os.system('say "Take a 5 minute break"')
+        print("After one hour")
+        self.run()
+
+class blink10(Thread):
+    def run(self):
+        sleep(60*20)
+        os.system('say "Look away and blink"')
+        sleep(20)
+        os.system('say "Close your eyes and take a deep breath"')
+        print("After 20 minutes")
+        self.run()
+
+
+print("Started...")
+os.system('say "Started"')
+
+fiveMinuteBreak().start()
+blink10().start()
+
+"""
+To launch at startup for OSX
+
+#!/bin/sh
+python3 {full path}/timer.py
+
+Place the above text in a file named as eye
+In System Preferences, Users and groupsm=, Login items, add this unix executable.
+
+"""
