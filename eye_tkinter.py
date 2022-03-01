09699374b0191e348dad7f5544724fc511c852cd
Susmit Vengurlekar <susmit.py@gmail.com>
2022-03-01 19:36:14 +0530

    Update eye_tkinter.py

--- eye_tkinter.py
+++ eye_tkinter.py
@@ -1,34 +1,62 @@
 from time import sleep
 from threading import Thread
 import tkinter as tk
+import os
 
-def get_window():
-    window = tk.Tk()
 
-    var = tk.StringVar()
-    var.set("Look Away Now")
-    lbl = tk.Label(window,textvariable=var,font=("Arial",30))
-    lbl.pack()
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.label = tk.Label(self, text="Look away now", font=("Arial", 30))
+        self.label.pack()
+        self.geometry("400x100")
 
-    window.geometry("400x100")
+    def set_look_away_text(self):
+        self.label["text"] = "Look away now"
 
-    return window
+    def set_break_text(self):
+        self.label["text"] = "Take a 5 minute break"
 
-window = get_window()
-window.withdraw()
+
+window = App()
+
 
 class closewindow(Thread):
     def run(self):
         sleep(3)
         window.withdraw()
 
-class blink20(Thread):
+
+class fiveMinuteBreak(Thread):
     def run(self):
-        sleep(20*60)
+        sleep(60 * 60)
+        os.system('spd-say "Take a 5 minute break"')
+        window.set_break_text()
         window.update()
         window.deiconify()
         closewindow().start()
         self.run()
 
+
+class blink20(Thread):
+    counter = 0
+
+    def run(self):
+        sleep(20 * 60)
+        self.counter += 1
+        if self.counter == 3:
+            self.counter = 0
+            self.run()
+        else:
+            window.set_look_away_text()
+            window.update()
+            window.deiconify()
+            closewindow().start()
+            os.system("spd-say 'Look Away Now'")
+            self.run()
+
+
+os.system("spd-say Started")
+fiveMinuteBreak().start()
 blink20().start()
 window.mainloop()
